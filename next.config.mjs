b3ff236e0580761dbe5a7ae9f68707b765e5d745
Tamir Enkhsaikhan <et4m1r@gmail.com>
2025-04-08 20:27:05 +1000

    fix: blob vercel hostname

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'img.freepik.com/**',
+        hostname: 'caxmltwcjw0u59uz.public.blob.vercel-storage.com',
       },
     ],
   },
